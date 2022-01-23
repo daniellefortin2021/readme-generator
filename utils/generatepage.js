a264599cba4b960f8ac1d0c7d9f1f11efcd83f25
Danielle Fortin <fortind6@gmail.com>
2022-01-23 17:09:24 -0500

    added readme template

--- utils/generatepage.js
+++ utils/generatepage.js
@@ -1,21 +1,46 @@
+const { title } = require("process");
+
 const generateReadMe = data => {
-    return `# ${data.title}
+    return `
+    
+    ## ${title}
     
     ## Description
     ${data.description}
 
-    ## Languages
+    ## Table of Contents
+    * [Languages](#languages)
+    * [Link](#link)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution](#contribution)
+    * [Tests](#tests)
+    * [License](#license)
+
+    ## Built With
+    This project was built with ${data.languages}
     
     ## Link
+    ${data.link}
 
     ## Installation
+    ${data.installation}
 
     ## Usage 
+    ${data.usage}
 
     ## Contribution
+    ${data.contribution}
 
     ## Tests
+    ${data.test}
 
     ## License 
+    This project is licensed under ${data.license}.
+
     `
 }
+
+console.log(data.title);
+
+module.exports = writeFile;
