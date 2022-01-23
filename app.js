cfe841c670e11c96863cdfeb4b9cf7d392a78465
Danielle Fortin <fortind6@gmail.com>
2022-01-23 16:21:59 -0500

    added prompt validation

--- app.js
+++ app.js
@@ -21,22 +21,54 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'What is your email address?'
+            message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'title',
-            message: 'What is your project name?'
+            message: 'What is your project name?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'link',
-            message: 'What is the link to your project repository?'
+            message: 'What is the link to your project repository?',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please add a link to your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description for your project.'
+            message: 'Provide a description for your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a short description for your project.');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -47,22 +79,47 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'installation',
-            message: 'What are the steps required to install your project?'
+            message: 'What are the steps required to install your project?',
+            validate: instructionInput => {
+                if (instructionInput) {
+                    return true;
+                } else {
+                    console.log('Please details steps needed to install your project.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'How do you use this app?'
+            message: 'How do you use this app?',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please detail how you to use this app.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'contribution',
-            message: 'What does the user need to know about contribution to the repo?'
+            message: 'What does the user need to know about contribution to the repo?',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('Please describe user contribution instructions.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'test',
-            message: 'What command should be run to run tests?'
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
         },
         {
             type: 'checkbox',
